7f57c884b42696bdb84af0005ed739b7dd4694d2
T.YAMASHITA <33086493+pyama2000@users.noreply.github.com>
2020-01-07 00:04:01 +0900

    Create main.yml

---
+++ .github/actions/release/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/ash
+set -eux
+
+TAG_NAME=`echo ${GITHUB_REF:10}`
+
+REQUEST=`curl -H "Authorization: token ${GITHUB_TOKEN}" \
+  -X POST https://api.github.com/repos/${GITHUB_REPOSITORY}/releases \
+  -d "
+  {
+    \"tag_name\": \"$TAG_NAME\",
+    \"target_commitish\": \"$GITHUB_SHA\",
+    \"name\": \"$FILE_NAME.pdf\",
+    \"draft\": false,
+    \"prerelease\": false
+  }"`
+
+RELEASE_ID=`echo ${REQUEST} | jq ".id"`
+
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+  -X POST https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=main.pdf \
+  --header 'Content-Type: application/pdf' \
+  --upload-file texfiles/${FILE_NAME}.pdf
